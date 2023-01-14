a3e15b75338fe282b69a58cfd87a92b3df7aef0c
iwnl-matt <orshan1003@gmail.com>
2023-01-14 18:34:35 +0300

    log btn fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AppRouter from './router/AppRouter';
 import AuthorizationPopup from './components/popups/authorizationPopus/AuthorizationPopup';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import UserCart from './components/userCart/UserCart';
 const App = () => {
   return (
@@ -10,13 +11,21 @@ const App = () => {
 =======
 import UserCard from './components/userCard/UserCard';
 import ProjectCard from './components/projectCard/ProjectCard';
+=======
+import UserCard from './components/ui/userCard/UserCard';
+import ProjectCard from './components/ui/projectCard/ProjectCard';
+>>>>>>> fe970c4 (log btn fix)
 const App = () => {
   return (
     <>
-      <ProjectCard/>
+      {/* <ProjectCard/> */}
       {/* <UserCard/> */}
+<<<<<<< HEAD
 >>>>>>> b6344c2 (projectCard added)
       {/* <AuthorizationPopup/> */}
+=======
+      <AuthorizationPopup/>
+>>>>>>> fe970c4 (log btn fix)
       <AppRouter />
     </>
   );
