99080baf79dcf9c4f84ba9fdc23d1a2a5c92f402
iwnl-matt <orshan1003@gmail.com>
2023-01-14 18:12:50 +0300

    projectCard added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import AppRouter from './router/AppRouter';
 import AuthorizationPopup from './components/popups/authorizationPopus/AuthorizationPopup';
+<<<<<<< HEAD
 import UserCart from './components/userCart/UserCart';
 const App = () => {
   return (
     <>
       <UserCart/>
+=======
+import UserCard from './components/userCard/UserCard';
+import ProjectCard from './components/projectCard/ProjectCard';
+const App = () => {
+  return (
+    <>
+      <ProjectCard/>
+      {/* <UserCard/> */}
+>>>>>>> b6344c2 (projectCard added)
       {/* <AuthorizationPopup/> */}
       <AppRouter />
     </>
